9241d85b67fc504f963d14645c860cbc3030bca8
funcylambda <lambda@edwardhaigh.com>
2018-09-27 18:02:13 +0100

    added 13.hs question

--- ProjectEuler/13.hs
+++ ProjectEuler/13.hs
@@ -1,3 +1,5 @@
+-- Work out the first ten digits of the sum of the following one-hundred 50-digit numbers (found in "13.txt")
+
 main = do
     filecontent <- readFile "13.txt"
     print (sum $ map read $ lines filecontent)
