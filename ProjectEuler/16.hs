f4f5138642c9d1420439bb3bc77d98730abf8b2d
funcylambda <lambda@edwardhaigh.com>
2018-09-27 19:57:10 +0100

    Not yet completed, but at least there are new files! #anyreasontocommit

---
+++ ProjectEuler/16.hs
@@ -0,0 +1,5 @@
+-- 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+-- What is the sum of the digits of the number 2^1000?
+
+digits :: Integer -> [Integer]
+digits n = map digitToInt n
